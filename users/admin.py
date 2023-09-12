ac80e7b51427e944801b90cdb975e87e19d12510
Nathan Martinez <27789402+nathandevx@users.noreply.github.com>
2023-09-12 15:41:16 -0700

    Minor changes
    Refactored the delete_user view and form associated with it. Edited the user admin.py model registrations.

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.conf import settings
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount, SocialToken, SocialApp
 from .models import User
 
 
 admin.site.register(User, UserAdmin)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialApp)
+
+if not settings.DEBUG:  # in production
+	admin.site.unregister(Group)
+	admin.site.unregister(EmailAddress)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
-from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import logout, get_user_model
 
-from users.forms import DeleteUser
+from users.forms import DeleteUserForm
 
 User = get_user_model()
 
@@ -13,23 +13,23 @@ def profile(request):
 
 
 def delete_user(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = DeleteUser(request.POST)
+    if request.user.is_anonymous or not request.user.is_authenticated:
+        return HttpResponseForbidden()
 
-            if form.is_valid():
-                if request.POST["delete_checkbox"]:
-                    rem = User.objects.get(username=request.user)
-                    if rem is not None:
-                        rem.delete()
-                        logout(request)
-                        messages.info(request, "Your account has been deleted.")
-                        return redirect(reverse("home:home"))
-                    else:
-                        messages.info(request, "There was an error.")
-        else:
-            form = DeleteUser()
-            context = {'form': form}
-            return render(request, 'users/delete_account.html', context)
-    if request.user.is_anonymous:
-        return HttpResponse(render(request, "error_pages/404.html"), status=404)
+    if request.method == 'POST':
+        form = DeleteUserForm(request.POST)
+
+        if form.is_valid():
+            if request.POST["delete_checkbox"]:
+                user = User.objects.get(username=request.user)
+                if user is not None:
+                    user.delete()  # delete the user account.
+                    logout(request)  # log them out.
+                    messages.info(request, "Your account has been deleted.")
+                    return redirect(reverse("home:home"))
+                else:
+                    messages.info(request, "There was an error.")
+    else:
+        form = DeleteUserForm()
+        context = {'form': form}
+        return render(request, 'users/delete_account.html', context)

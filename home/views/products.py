ba975d56ec99a3a8870f73d2ca4afc5077eb6df5
rahilpatel29 <rahilpatel@csus.edu>
2023-10-15 22:34:57 -0700

    DT- 102

--- home/views/products.py
+++ home/views/products.py
@@ -1,22 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponseForbidden
 from senior_project.utils import superuser_required
+from ..models import Product
+from ..forms import ProductForm
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 
 @superuser_required
 def product_create(request):
-	return render(request, 'home/products/create.html')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            # You can integrate the stripe logic here if needed
+            return redirect(product.get_read_url())  # redirecting to product detail page
+    else:
+        form = ProductForm()
+    return render(request, 'home/product/create.html', {'form': form})
 
 
-def product_read(request):
-	return render(request, 'home/products/read.html')
+def product_read(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'home/product/read.html', {'product': product})
 
 
 @superuser_required
-def product_update(request):
-	return render(request, 'home/products/update.html')
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            product = form.save()
+            # You can integrate the stripe logic here if needed
+            return redirect(product.get_read_url())  # redirecting to product detail page
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'home/product/update.html', {'form': form, 'product': product})
+
 
 
 @superuser_required
-def product_delete(request):
-	return render(request, 'home/products/delete.html')
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete_product_and_set_stripe_product_as_inactive()  # This method handles your custom deletion logic
+        return redirect('home:product-list')
+    return render(request, 'home/product/delete.html', {'product': product})
+
+

a16c013be2747037c603aceafe9a7fa3021295c0
Nathan Martinez <nathanzeroday@gmail.com>
2023-12-23 20:57:25 -0800

    admin page edits

--- home/admin.py
+++ home/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.conf import settings
 from .models import Product, ProductImage, ShippingAddress, Cart, CartItem, Order, OrderHistory
 
 
-# The models shouldn't be editable via admin in production
-if settings.DEBUG:  # if in development
-	admin.site.register(Product)
-	admin.site.register(ProductImage)
-	admin.site.register(ShippingAddress)
-	admin.site.register(Cart)
-	admin.site.register(CartItem)
-	admin.site.register(Order)
-	admin.site.register(OrderHistory)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ShippingAddress)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderHistory)
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
-from django.conf import settings
-from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialAccount, SocialToken, SocialApp
 from .models import User
 
@@ -11,7 +8,3 @@ admin.site.register(User, UserAdmin)
 admin.site.unregister(SocialAccount)
 admin.site.unregister(SocialToken)
 admin.site.unregister(SocialApp)
-
-if not settings.DEBUG:  # in production
-	admin.site.unregister(Group)
-	admin.site.unregister(EmailAddress)

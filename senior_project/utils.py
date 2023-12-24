1f45c03faf50cbffe49de2b1e1f1a71a84d93047
Nathan Martinez <nathanzeroday@gmail.com>
2023-12-24 14:40:41 -0800

    Small changes

--- senior_project/utils.py
+++ senior_project/utils.py
@@ -170,9 +170,8 @@ def email_num_dummy_users():
 	"""
 	Sends an email to the admin if the number of dummy users in use (ADMIN or CUSTOMER groups) is greater than 1.
 	"""
-	num_admins_in_use = get_num_available_dummy_users("ADMIN")
-	num_customers_in_use = get_num_available_dummy_users("CUSTOMER")
-	if (num_admins_in_use > 2) or (num_customers_in_use > 2):
-		send_mail("SP: Demo Users", f"{num_admins_in_use} of admins are in use. {num_customers_in_use} of customers are in use.", env('ADMIN_EMAIL'), [env('ADMIN_EMAIL')])
-
-
+	if settings.DEBUG is False:  # if in production
+		num_admins_in_use = get_num_available_dummy_users("ADMIN")
+		num_customers_in_use = get_num_available_dummy_users("CUSTOMER")
+		if (num_admins_in_use > 2) or (num_customers_in_use > 2):
+			send_mail("SP: Demo Users", f"{num_admins_in_use} of admins are in use. {num_customers_in_use} of customers are in use.", env('ADMIN_EMAIL'), [env('ADMIN_EMAIL')])
